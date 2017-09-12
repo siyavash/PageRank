debb1b11f7921f55665866280773136378053e4f
Siyavash Najafzade <siyavash.najafzade@gmail.com>
2017-09-12 10:21:45 +0430

    create SparkConf object and set app name and master

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -1,2 +1,7 @@
+import org.apache.spark.SparkConf;
+
 public class PageRank {
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setAppName("PageRank").setMaster("spark://master:7077");
+    }
 }
