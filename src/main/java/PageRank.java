3e1e56549dfd190732dc51526a6cadbe80d985d2
Siyavash Najafzade <siyavash.najafzade@gmail.com>
2017-09-12 10:23:51 +0430

    create hbase configuration and set hbase configs and zookeeper properties

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -1,7 +1,21 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class PageRank {
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf().setAppName("PageRank").setMaster("spark://master:7077");
+
+        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
+
+        Configuration hbaseConfiguration = HBaseConfiguration.create();
+
+        hbaseConfiguration.set("hbase.master", "master:60000");
+        hbaseConfiguration.set("zookeeper.znode.parent", "/hbase");
+        hbaseConfiguration.set("hbase.zookeeper.property.clientPort", "2181");
+        hbaseConfiguration.set("hbase.zookeeper.quorum", "master,slave");
+        hbaseConfiguration.set(TableInputFormat.INPUT_TABLE, "wb");
     }
 }
