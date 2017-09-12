7fbd812f0e7b8f4207119efaa47c73b3b6791348
Siyavash Najafzade <siyavash.najafzade@gmail.com>
2017-09-12 10:45:01 +0430

    implement call method for mapToPair PairFunction class for making web graph RDD from hbase data

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -12,6 +12,10 @@ import scala.Tuple2;
 import java.util.ArrayList;
 
 public class PageRank {
+
+    private static final byte[] SL_COLUMN_FAMILY = "sl".getBytes();
+    private static final byte[] SUB_LINKS_COLUMN = "subLinks".getBytes();
+
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf().setAppName("PageRank").setMaster("spark://master:7077");
 
@@ -35,7 +39,24 @@ public class PageRank {
                 new PairFunction<Tuple2<ImmutableBytesWritable, Result>, String, ArrayList<String>>() {
                     public Tuple2<String, ArrayList<String>> call(Tuple2<ImmutableBytesWritable,
                                                                   Result> tuple2) throws Exception {
-                        return null;
+                        String subLinks = null;
+                        try {
+                            subLinks = new String(tuple2._2.getValue(SL_COLUMN_FAMILY, SUB_LINKS_COLUMN));
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        ArrayList<String> outputLinks = new ArrayList<String>();
+
+                        if(subLinks != null) {
+                            String[] urls = subLinks.split("\n");
+                            for(int i = 0; i < urls.length; i += 2) {
+                                outputLinks.add(urls[i]);
+                            }
+                        }
+
+                        return new Tuple2<String, ArrayList<String>>(new String(tuple2._1.copyBytes()), outputLinks);
                     }
                 }
         );
